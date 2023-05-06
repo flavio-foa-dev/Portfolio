e21fd5bd7d93b780c458626261fce09294af0188
Flavio Andrade <foa12381@gmail.com>
2023-05-05 21:19:57 -0300

    types

--- src/interfaces/Idata.ts
+++ src/interfaces/Idata.ts
@@ -1,10 +1,10 @@
 export default interface Idata {
-  id: number
-  favorite: boolean,
-  title: string,
-  type: string,
-  img: string,
-  github: string,
-  site: string,
-  youTube: string,
+  id?: number
+  favorite?: boolean,
+  title?: string,
+  type?: string,
+  img?: string,
+  github?: string,
+  site?: string,
+  youTube?: string,
 }
