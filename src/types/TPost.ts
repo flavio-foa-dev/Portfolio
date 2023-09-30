ff77b90005c2089174fe5f6b19ffde434b0fcd9c
Flavio Andrade <foa12381@gmail.com>
2023-09-29 23:19:59 -0300

    refactor/data/json, favicon, post

---
+++ src/types/TPost.ts
@@ -0,0 +1,11 @@
+export type TPost = {
+  id: number,
+  title: string,
+  type: string,
+  img: string,
+  favorite: boolean,
+  github: string,
+  site: string,
+  youTube: string,
+  tools: string
+}
